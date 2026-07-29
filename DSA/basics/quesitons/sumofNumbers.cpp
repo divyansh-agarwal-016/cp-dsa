014eef00d4671b66cbf89667f4d107c4107c8190
divyanshagarwal <divyanshagarwal026@gmail.com>
2026-07-29 15:47:16 +0530

    Basic Question Practice

---
+++ DSA/basics/quesitons/sumofNumbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit;
+            n /= 10;
+        }
+
+        cout << sum << endl;
+    }
+
+    return 0;
+}
---
+++ DSA/basics/quesitons/tempCodeRunnerFile.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    while(T--){
+        int n;
+        cin >> n;
+    
+        int reverse = 0;
+    
+        while (n > 0) {
+            int lastDigit = n % 10;
+            reverse = reverse * 10 + lastDigit;
+            n /= 10;
+        }
+    
+        cout << reverse << endl;
+    
+        return 0;
+    }
+
+}
